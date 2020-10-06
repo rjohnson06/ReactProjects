b49d02d13bc5cb41e4500b239d702e37639d5d36
rjohnson06 <ryan.johnson782@gmail.com>
2020-10-05 23:32:09 -0400

    gumbo space

--- gumbo-space/gumbo-space/src/components/Map/Booker/Booker.js
+++ gumbo-space/gumbo-space/src/components/Map/Booker/Booker.js
@@ -2,16 +2,13 @@ import React from 'react';
 import classes from './Booker.module.css';
 
 import Aux from '../../../hox/Auxilliary';
+import Modal from '../../UI/Modal/Modal';
 
 const booker = (props) => {
-  const backdrop = props.show ? <div className={classes.bookerBackdrop}></div> : null;
-  const modal = props.show ? <div className={classes.booker}></div> : null;
-
   return (
-    <Aux>
-      {modal}
-      {backdrop}
-    </Aux>
+    <Modal classes={classes.bookerModal} show={props.show}>
+      <h2>Book Desk Time</h2>
+    </Modal>
   );
 };
 
